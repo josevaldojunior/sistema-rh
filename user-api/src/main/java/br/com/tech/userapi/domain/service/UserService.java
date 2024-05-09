61fed47c93227d381536b910108b0b8d650d7c92
josevaldojunior <jjrsmedeiros@gmail.com>
2024-05-09 00:17:22 -0300

    feat: api gateway created

--- user-api/src/main/java/br/com/tech/userapi/domain/service/UserService.java
+++ user-api/src/main/java/br/com/tech/userapi/domain/service/UserService.java
@@ -4,17 +4,22 @@ import br.com.tech.userapi.domain.exception.EntityNotFoundException;
 import br.com.tech.userapi.domain.model.User;
 import br.com.tech.userapi.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final Environment environment;
     private final UserRepository userRepository;
 
     public User findById(Long id){
+        log.info("USER_SERVICE ::: Get request on " + environment.getProperty("local.server.port") + " port");
         return userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
     }
